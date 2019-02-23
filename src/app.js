891e74c78c07b5e157d3ec4ec0caaa0bb94112e9
Christopher <christopher.carnivale@gmail.com>
2019-02-23 14:37:07 -0500

    added response message to POST

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config')
+const { NODE_ENV } = require('./config');
+const uuid= require('uuid/v4'); 
 
 const app = express();
 
@@ -14,12 +15,111 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(express.json()); 
 app.use(helmet());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!');
+const users = [{
+  'id': '3c8da4d5-1597-46e7-baa1-e402aed70d80',
+  'username': 'sallyStudent',
+  'password': 'c00d1ng1sc00l',
+  'favoriteClub': 'Cache Valley Stone Society',
+  'newsLetter': 'true'
+},
+{
+  'id': 'ce20079c-2326-4f17-8ac4-f617bfd28b7f',
+  'username': 'johnBlocton',
+  'password': 'veryg00dpassw0rd',
+  'favoriteClub': 'Salt City Curling Club',
+  'newsLetter': 'false'
+}]; 
+
+app.post('/register', (req, res) => {
+  const { username, password, favoriteClub, newsLetter=false } = req.body; 
+  const clubs = [
+    'Cache Valley Stone Society',
+    'Ogden Curling Club',
+    'Park City Curling Club',
+    'Salt City Curling Club',
+    'Utah Olympic Oval Curling Club'
+  ];
+
+  if (!username) {
+    return res
+      .status(400)
+      .send('Username required');
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .send('Passowrd required');
+  }
+
+  if(!favoriteClub) {
+    return res
+      .staus(400)
+      .send('favorite Club required'); 
+  }
+
+  if (username.length < 6 || username.length > 20){
+    return res
+      .status(400)
+      .send('Username must be between 6 and 20 characters'); 
+  }
+
+  if (password.length < 8 || password.length > 36) {
+    return res 
+      .status(400)
+      .send ('Password must be between 8 and 36 characters');
+  }
+
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)){
+    return res 
+      .staus(400)
+      .send('Password must contain at least one digit'); 
+  }
+
+  if(!clubs.includes(favoriteClub)){
+    return res
+      .status(400)
+      .send('Not a valid club'); 
+  }
+
+  const id = uuid(); 
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter
+  }; 
+
+  users.push(newUser);
+  res.status(201).location(`http://localhost:8000/user/${id}`).json({id:id});  
 });
+
+app.delete('/user/:userId', (req, res) => {
+  const { userId } = req.params;
+  const index = users.findIndex(u => u.id === userId); 
+
+  if (index === -1){
+    return res.
+      status(404)
+      .send('User not found');
+  }
+
+  users.splice(index, 1);
+
+  res.staus(204)
+    .end();
+}); 
+
+
+app.get('/user', (req, res) => {
+  return res.json(users);
+});
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
